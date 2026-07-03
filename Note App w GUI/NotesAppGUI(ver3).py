3cd537de110cf95190652a1bb23d1da18629e863
Charles Vence Espinas <charlesvenceespinas@gmail.com>
2026-07-03 23:34:05 +0800

    Update NotesAppGUI(ver3).py

--- Note App w GUI/NotesAppGUI(ver3).py
+++ Note App w GUI/NotesAppGUI(ver3).py
@@ -1,103 +1,96 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QTextEdit, QVBoxLayout, QHBoxLayout, QMessageBox, QFileDialog
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout, QVBoxLayout
 from PyQt6.QtCore import Qt
 
 app = QApplication(sys.argv)
 
 window = QWidget()
-window.setWindowTitle("NotePad")
 
 mainLayout = QVBoxLayout()
 
 header = QVBoxLayout()
-top = QLabel("--------------------------------------------------")
+top = QLabel("--------------------")
 top.setAlignment(Qt.AlignmentFlag.AlignCenter)
-title = QLabel("    Notepad    ")
+title = QLabel("Calculator")
 title.setAlignment(Qt.AlignmentFlag.AlignCenter)
-bottom = QLabel("--------------------------------------------------")
+bottom = QLabel("--------------------")
 bottom.setAlignment(Qt.AlignmentFlag.AlignCenter)
+display = QLineEdit()
+
 header.addWidget(top)
 header.addWidget(title)
 header.addWidget(bottom)
+header.addWidget(display)
 
-body = QVBoxLayout()
-note = QTextEdit()
-body.addWidget(note)
+buttons = QGridLayout()
+button7 = QPushButton("7")
+button8 = QPushButton("8")
+button9 = QPushButton("9")
+buttonD = QPushButton("/")
+button4 = QPushButton("4")
+button5 = QPushButton("5")
+button6 = QPushButton("6")
+buttonM = QPushButton("*")
+button1 = QPushButton("1")
+button2 = QPushButton("2")
+button3 = QPushButton("3")
+buttonS = QPushButton("-")
+buttonC = QPushButton("C")
+button0 = QPushButton("0")
+buttonE = QPushButton("=")
+buttonA = QPushButton("+")
 
-actions = QHBoxLayout()
-new = QPushButton("New")
-save = QPushButton("Save")
-load = QPushButton("Load")
-clear = QPushButton("Clear")
-actions.addWidget(new)
-actions.addWidget(save)
-actions.addWidget(load)
-actions.addWidget(clear)
+buttons.addWidget(button7, 0, 0)
+buttons.addWidget(button8, 0, 1)
+buttons.addWidget(button9, 0, 2)
+buttons.addWidget(buttonD, 0, 3)
+buttons.addWidget(button4, 1, 0)
+buttons.addWidget(button5, 1, 1)
+buttons.addWidget(button6, 1, 2)
+buttons.addWidget(buttonM, 1, 3)
+buttons.addWidget(button1, 2, 0)
+buttons.addWidget(button2, 2, 1)
+buttons.addWidget(button3, 2, 2)
+buttons.addWidget(buttonS, 2, 3)
+buttons.addWidget(buttonC, 3, 0)
+buttons.addWidget(button0, 3, 1)
+buttons.addWidget(buttonE, 3, 2)
+buttons.addWidget(buttonA, 3, 3)
 
-status = QLabel("Ready")
-characterCounter = QLabel(f"Characters : ")
-wordCounter = QLabel(f"Words: ")
-
-def updateCounter():
-    text = note.toPlainText()
-    characterCountWithoutSpace = len(text.replace(" ", "").strip())
-    wordCounterWithoutSpace = len(text.split())
-    characterCounter.setText(f"Characters : {characterCountWithoutSpace}")
-    wordCounter.setText(f"Words: {wordCounterWithoutSpace}")
-    
-def nw():
-    note.clear()
-    status.setText("Ready")
-    updateCounter()
-
-
-def sv():
-    filename, _ = QFileDialog.getSaveFileName(window, "Save a File", "", "Text Files (*.txt);;Python Files (*.py);;CPP Files (*.cpp);;All Files (*)")
-    if not filename:
-        return
-    with open (filename, "w") as f:
-        text = note.toPlainText()
-        f.write(text)
-        status.setText("Note Saved!") 
-        updateCounter()
-
-def ld():
-    filename, _ = QFileDialog.getOpenFileName(window, "Open a File", "", "Text Files (*.txt);;Python Files (*.py);;CPP Files (*.cpp);;All Files (*)")
-    if not filename:
-        return
-    try:
-        with open(filename, "r") as g:
-            content = g.read()
-            note.setPlainText(content)
-            status.setText("Note Loaded!")
-            updateCounter()
-    except FileNotFoundError:
-        status.setText("File Does Not Exist")
-        updateCounter()
+def pressButton(number):
+    currentDisplay = display.text()
+    display.setText(currentDisplay + number)
 
 def clr():
-    confirmation = QMessageBox.question(window, "Confirmation Required", "Are you sure?", QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
-    if confirmation == QMessageBox.StandardButton.Yes:
-        note.clear()
-        status.setText("Note Cleared!")
-        updateCounter()
-    elif confirmation == QMessageBox.StandardButton.No:
-        return
-    
-new.clicked.connect(nw)
-save.clicked.connect(sv)
-load.clicked.connect(ld)
-clear.clicked.connect(clr)
+    display.clear()
+
+
+
+button7.clicked.connect(lambda: pressButton("7"))
+button8.clicked.connect(lambda: pressButton("8"))
+button9.clicked.connect(lambda: pressButton("9"))
+buttonD.clicked.connect(lambda: pressButton("/"))
+button4.clicked.connect(lambda: pressButton("4"))
+button5.clicked.connect(lambda: pressButton("5"))
+button6.clicked.connect(lambda: pressButton("6"))
+buttonM.clicked.connect(lambda: pressButton("*"))
+button1.clicked.connect(lambda: pressButton("1"))
+button2.clicked.connect(lambda: pressButton("2"))
+button3.clicked.connect(lambda: pressButton("3"))
+buttonS.clicked.connect(lambda: pressButton("-"))
+button0.clicked.connect(lambda: pressButton("0"))
+buttonA.clicked.connect(lambda: pressButton("+"))
+
+buttonC.clicked.connect(clr)
+
+#buttonE
+
+
 
 mainLayout.addLayout(header)
-mainLayout.addWidget(status)
-mainLayout.addLayout(body)
-mainLayout.addWidget(characterCounter)
-mainLayout.addWidget(wordCounter)
-mainLayout.addLayout(actions)
+mainLayout.addLayout(buttons)
 
 window.setLayout(mainLayout)
 
 window.show()
 app.exec()
- 
