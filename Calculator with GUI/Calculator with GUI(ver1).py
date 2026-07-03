3d687d8b594d48acaf03f008d7cb4389c694d8fc
Charles Vence Espinas <charlesvenceespinas@gmail.com>
2026-07-03 23:35:06 +0800

    Update Calculator with GUI(ver1).py

--- Calculator with GUI/Calculator with GUI(ver1).py
+++ Calculator with GUI/Calculator with GUI(ver1).py
@@ -23,27 +23,67 @@ header.addWidget(bottom)
 header.addWidget(display)
 
 buttons = QGridLayout()
-button7 = buttons.addWidget(QPushButton("7"), 0, 0)
-button8 = buttons.addWidget(QPushButton("8"), 0, 1)
-button9 = buttons.addWidget(QPushButton("9"), 0, 2)
-buttonD = buttons.addWidget(QPushButton("/"), 0, 3)
-button4 = buttons.addWidget(QPushButton("4"), 1, 0)
-button5 = buttons.addWidget(QPushButton("5"), 1, 1)
-button6 = buttons.addWidget(QPushButton("6"), 1, 2)
-buttonM = buttons.addWidget(QPushButton("*"), 1, 3)
-button1 = buttons.addWidget(QPushButton("1"), 2, 0)
-button2 = buttons.addWidget(QPushButton("2"), 2, 1)
-button3 = buttons.addWidget(QPushButton("3"), 2, 2)
-buttonS = buttons.addWidget(QPushButton("-"), 2, 3)
-buttonC = buttons.addWidget(QPushButton("C"), 3, 0)
-button0 = buttons.addWidget(QPushButton("0"), 3, 1)
-ButtonE = buttons.addWidget(QPushButton("="), 3, 2)
-buttonA = buttons.addWidget(QPushButton("+"), 3, 3)
+button7 = QPushButton("7")
+button8 = QPushButton("8")
+button9 = QPushButton("9")
+buttonD = QPushButton("/")
+button4 = QPushButton("4")
+button5 = QPushButton("5")
+button6 = QPushButton("6")
+buttonM = QPushButton("*")
+button1 = QPushButton("1")
+button2 = QPushButton("2")
+button3 = QPushButton("3")
+buttonS = QPushButton("-")
+buttonC = QPushButton("C")
+button0 = QPushButton("0")
+buttonE = QPushButton("=")
+buttonA = QPushButton("+")
 
-def pressButton():
-    
+buttons.addWidget(button7, 0, 0)
+buttons.addWidget(button8, 0, 1)
+buttons.addWidget(button9, 0, 2)
+buttons.addWidget(buttonD, 0, 3)
+buttons.addWidget(button4, 1, 0)
+buttons.addWidget(button5, 1, 1)
+buttons.addWidget(button6, 1, 2)
+buttons.addWidget(buttonM, 1, 3)
+buttons.addWidget(button1, 2, 0)
+buttons.addWidget(button2, 2, 1)
+buttons.addWidget(button3, 2, 2)
+buttons.addWidget(buttonS, 2, 3)
+buttons.addWidget(buttonC, 3, 0)
+buttons.addWidget(button0, 3, 1)
+buttons.addWidget(buttonE, 3, 2)
+buttons.addWidget(buttonA, 3, 3)
+
+def pressButton(number):
+    currentDisplay = display.text()
+    display.setText(currentDisplay + number)
+
+def clr():
+    display.clear()
 
 
+
+button7.clicked.connect(lambda: pressButton("7"))
+button8.clicked.connect(lambda: pressButton("8"))
+button9.clicked.connect(lambda: pressButton("9"))
+buttonD.clicked.connect(lambda: pressButton("/"))
+button4.clicked.connect(lambda: pressButton("4"))
+button5.clicked.connect(lambda: pressButton("5"))
+button6.clicked.connect(lambda: pressButton("6"))
+buttonM.clicked.connect(lambda: pressButton("*"))
+button1.clicked.connect(lambda: pressButton("1"))
+button2.clicked.connect(lambda: pressButton("2"))
+button3.clicked.connect(lambda: pressButton("3"))
+buttonS.clicked.connect(lambda: pressButton("-"))
+button0.clicked.connect(lambda: pressButton("0"))
+buttonA.clicked.connect(lambda: pressButton("+"))
+
+buttonC.clicked.connect(clr)
+
+#buttonE
 
 
 
