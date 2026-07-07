af11facc1e88a1869030b3c93f70d8cf659a6128
Charles Vence Espinas <charlesvenceespinas@gmail.com>
2026-07-08 01:38:56 +0800

    Add files via upload

---
+++ calculator(console)/Calculator console PEMDAS(ver2).py
@@ -0,0 +1,42 @@
+print("----------------------------------------")
+print("Console Calculator".center(40))
+print("----------------------------------------")
+
+while True:
+    expression = input("Enter an expression: (type 'exit' to stop): ") 
+    if expression == "exit":
+        break
+    parts = expression.split()
+    
+    
+    while "*" in parts or "/" in parts:
+            for i  in range(1, len(parts), 2):
+                operator = parts[i]
+                leftNumber = float(parts[i - 1])
+                rightNumber = float(parts[i + 1])
+
+                if operator == "*" or operator == "/":
+                    if operator == "*":
+                        answer = leftNumber*rightNumber
+                        parts[i-1:i+2] = [str(answer)]
+                    else:
+                        try:
+                            answer = leftNumber/rightNumber
+                            parts[i-1:i+2] = [str(answer)]
+                        except ZeroDivisionError:
+                            print("Math Error! Enter another expression without dividing 0")
+
+    while "+" in parts or "-" in parts:
+       for i  in range(1, len(parts), 2):
+                operator = parts[i]
+                leftNumber = float(parts[i - 1])
+                rightNumber = float(parts[i + 1])
+
+                if operator == "+" or operator == "-":
+                    if operator == "+":
+                        answer = leftNumber+rightNumber
+                        parts[i-1:i+2] = [str(answer)]
+                    else:
+                        answer = leftNumber-rightNumber
+                        parts[i-1:i+2] = [str(answer)]
+                       
