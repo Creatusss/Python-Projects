2143e946c95afaad212f01f6feecc0d635343133
Charles Vence Espinas <charlesvenceespinas@gmail.com>
2026-07-08 23:45:13 +0800

    Update Calculator console PEMDAS(ver2).py

--- calculator(console)/Calculator console PEMDAS(ver2).py
+++ calculator(console)/Calculator console PEMDAS(ver2).py
@@ -10,12 +10,15 @@ while True:
     
     
     while "*" in parts or "/" in parts:
-            for i  in range(1, len(parts), 2):
-                operator = parts[i]
-                leftNumber = float(parts[i - 1])
-                rightNumber = float(parts[i + 1])
+        for i  in range(1, len(parts), 2):
+            operator = parts[i]
+            leftNumber = float(parts[i - 1])
+            if i + 1 >= len(parts):
+                print("Invalid Expression!")
+                break
+            rightNumber = float(parts[i + 1])
 
-                if operator == "*" or operator == "/":
+            if operator == "*" or operator == "/":
                     if operator == "*":
                         answer = leftNumber*rightNumber
                         parts[i-1:i+2] = [str(answer)]
@@ -25,18 +28,30 @@ while True:
                             parts[i-1:i+2] = [str(answer)]
                         except ZeroDivisionError:
                             print("Math Error! Enter another expression without dividing 0")
+        
+            break
+                        
+                
 
     while "+" in parts or "-" in parts:
-       for i  in range(1, len(parts), 2):
-                operator = parts[i]
-                leftNumber = float(parts[i - 1])
-                rightNumber = float(parts[i + 1])
+        for i in range(1, len(parts), 2):   
+            operator = parts[i]
+            leftNumber = float(parts[i - 1])
+            if i + 1 >= len(parts):
+                print("Invalid Expression!")
+                break
+            rightNumber = float(parts[i + 1])
 
-                if operator == "+" or operator == "-":
+            if operator == "+" or operator == "-":
                     if operator == "+":
                         answer = leftNumber+rightNumber
                         parts[i-1:i+2] = [str(answer)]
                     else:
                         answer = leftNumber-rightNumber
                         parts[i-1:i+2] = [str(answer)]
+            break
+                    
+        
+
+    print(parts[0])
                        
