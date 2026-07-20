ca3ab0fd686d65e721c3beda4d146b7d5fef2072
Charles Vence Espinas <charlesvenceespinas@gmail.com>
2026-07-20 20:38:18 +0800

    Update Expense Tracker.py

--- Expense Tracker with GUI/Expense Tracker.py
+++ Expense Tracker with GUI/Expense Tracker.py
@@ -57,37 +57,51 @@ outputBar.addWidget(status)
 
 
 def addFeature():
-    userExpense = expense.text()
+    userExpense = expense.text().strip()
+    for existingExpense, existingAmount in expensesList:
+        if userExpense.lower() == existingExpense.lower():
+            status.setText("Warning: Duplicate Expense")
+            expense.clear()
+            amount.clear()
+            return
     try:
         userAmount = int(amount.text())
+        if not userExpense or userAmount <= 0:
+            raise ValueError
+        
         status.setText("Status: Ready")
-        item = f"{userExpense} - ₱{userAmount}"
 
         global totalPrice
         totalPrice += userAmount
 
-        expensesList.append(item)
-        expensesListGUI.addItem(item)
+        expensesList.append((userExpense, userAmount))#Tuple so that the individual records are immutable, also so that it can be as one object
+        expensesListGUI.addItem(f"{userExpense} - ₱{userAmount}")
 
         expense.clear()
         amount.clear()
 
-        status.setText("Added")
-        outputTotalExpenses.setText(f"Total Expenses: {totalPrice}")
+        status.setText("Status: Added")
+        outputTotalExpenses.setText(f"Total Expenses: ₱{totalPrice}")
 
     except ValueError:
-        status.setText("Invalid Input")
+        status.setText("Warning: Invalid Input")
 
-    
 
 def delFeature():
     deleteRow = expensesListGUI.currentRow()
     if deleteRow >= 0:
+        userExpense, userAmount = expensesList[deleteRow]
+
+        global totalPrice
+        totalPrice -= userAmount 
+
         expensesList.pop(deleteRow)
         expensesListGUI.takeItem(deleteRow)
-        status.setText("Deleted")
+
+        status.setText("Status: Deleted")
+        outputTotalExpenses.setText(f"Total Expenses: ₱{totalPrice}")
     else:
-        status.setText("Please Select a Task.")
+        status.setText("Warning: Please Select a Task.")
     
 
 addExpense.clicked.connect(addFeature)
